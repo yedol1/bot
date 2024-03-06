968cfa6e206c974b6952c0701f2398596b184f1c
10_2pang <57481378+yedol1@users.noreply.github.com>
2024-03-07 01:45:18 +0900

    refactor: file

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-const { App } = require('@slack/bolt');
+const { App } = require("@slack/bolt");
+require("dotenv").config();
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
 });
 
 // hello 라는 메세지가 오는 것을 감지하는 부분입니다.
-app.message('hello', async ({ message, say }) => {
+app.message("hello", async ({ message, say }) => {
   // say() 는 block 이라고 하는 슬랙 메세지 형태의 일종을 보내는 함수입니다. blocks 를 설정해서 보내면 됩니다.
   await say({
     blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`,
         },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
           },
-          "action_id": "button_click"
-        }
-      }
+          action_id: "button_click",
+        },
+      },
     ],
-    text: `Hey there <@${message.user}>!`
+    text: `Hey there <@${message.user}>!`,
   });
 });
 
-app.action('button_click', async ({ body, ack, say }) => {
+app.action("button_click", async ({ body, ack, say }) => {
   // Acknowledge the action
   await ack();
   await say(`<@${body.user.id}> clicked the button`);
@@ -43,5 +44,5 @@ app.action('button_click', async ({ body, ack, say }) => {
   // Start your app
   await app.start();
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
